9dbdf7b9be6537b64fe5013ba880e658c4f93fda
karolef <34057620+karolef@users.noreply.github.com>
2018-01-13 21:34:45 +0100

    Update ship.js

--- Asteroids/empty-example/ship.js
+++ Asteroids/empty-example/ship.js
@@ -25,12 +25,21 @@ function Ship() {
     this.vel.add(force);
   }
 
+  this.hits = function(asteroid){
+    let d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
+    if (d < this.r + asteroid.r){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   this.render = function() {
     push();
     translate(this.pos.x, this.pos.y);
     rotate(this.heading + PI / 2);
+    fill(0);
     stroke(255);
-    noFill();
     triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
     pop();
   }
