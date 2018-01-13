0114db7a549d60d68263c6240f2afc350c123fb2
karolef <34057620+karolef@users.noreply.github.com>
2018-01-13 21:33:49 +0100

    Update asteroids.js

--- Asteroids/empty-example/asteroids.js
+++ Asteroids/empty-example/asteroids.js
@@ -1,12 +1,23 @@
-function Asteroid() {
-  this.pos = createVector(random(width), random(height));
+function Asteroid(pos, r) {
+  if (pos) {
+    this.pos = pos.copy();
+  } else {
+    this.pos = createVector(random(width), random(height));
+  }
+
+  if (r){
+    this.r = r * 0.5;
+  } else {
+    this.r = random(15, 50);
+  }
+
   this.vel = p5.Vector.random2D();
-  this.r = random(15, 50);
+
   this.total = floor(random(5, 15));
   this.offset = [];
 
   for (let i = 0; i < this.total; i++) {
-    this.offset[i] = random(-15, 15);
+    this.offset[i] = random(-this.r * 0.5, this.r*0.5);
   }
 
   this.update = function() {
@@ -18,7 +29,6 @@ function Asteroid() {
     translate(this.pos.x, this.pos.y);
     stroke(255);
     noFill();
-    //ellipse(0,0,this.r * 2);
 
     beginShape();
     for (let i = 0; i < this.total; i++) {
@@ -30,19 +40,26 @@ function Asteroid() {
     }
     endShape(CLOSE);
     pop();
+  }
 
-    this.edges = function() {
-      if (this.pos.x > width + this.r) {
-        this.pos.x = -this.r;
-      } else if (this.pos.x < -this.r) {
-        this.pos.x = width + this.r;
-      }
+  this.breakup = function() {
+    let newA = [];
+    newA[0] = new Asteroid(this.pos, this.r);
+    newA[1] = new Asteroid(this.pos, this.r);
+    return newA;
+  }
 
-      if (this.pos.y > height + this.r) {
-        this.pos.y = -this.r;
-      } else if (this.pos.y < -this.r) {
-        this.pos.y = height + this.r;
-      }
+  this.edges = function() {
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    } else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+    }
+
+    if (this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
     }
   }
 }
