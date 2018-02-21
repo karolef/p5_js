cac469f74bfff01086a62962f3f01ab8007993d9
karolef <34057620+karolef@users.noreply.github.com>
2018-02-21 22:27:31 +0100

    Steering behavior with evolution
    An organism of sorts is displaying steering behavior - eating food (green dot) to live and sustain itself. If it does not eat or eats poison (red dot) his health will drop and will die. The genetic algorithm will be added later to let the populations or survivors rather give birth to new better organisms

---
+++ SteeringBehaviorWithEvolution/sketch.js
@@ -0,0 +1,56 @@
+let vehicles = [];
+let food = [];
+let poison = [];
+
+function setup() {
+  createCanvas(600, 600);
+  for (let i = 0; i < 10; i++) {
+    let x = random(width);
+    let y = random(height);
+    vehicles[i] = new Vehicle(x, y);
+  }
+
+  for (let i = 0; i < 50; i++) {
+    let x = random(width);
+    let y = random(height);
+    food.push(createVector(x, y));
+  }
+
+  for (let i = 0; i < 10; i++) {
+    let x = random(width);
+    let y = random(height);
+    poison.push(createVector(x, y));
+  }
+}
+
+function draw() {
+  background(51);
+
+  if (random(1) < 0.1) {
+    let x = random(width);
+    let y = random(height);
+    food.push(createVector(x, y));
+  }
+
+  for (let i = 0; i < food.length; i++) {
+    fill(0, 255, 0);
+    noStroke();
+    ellipse(food[i].x, food[i].y, 8, 8);
+  }
+
+  for (let i = 0; i < poison.length; i++) {
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(poison[i].x, poison[i].y, 8, 8);
+  }
+
+  for (let i = vehicles.length - 1; i >= 0; i--) {
+    vehicles[i].behaviors(food, poison);
+    vehicles[i].update();
+    vehicles[i].display();
+
+    if (vehicles[i].dead()) {
+      vehicles.splice(i, 1);
+    }
+  }
+}
