3b2321d93411a496fbd98dca03500779c2e73311
karolef <34057620+karolef@users.noreply.github.com>
2018-02-22 15:57:20 +0100

    updating sketch.js
    adding adjustments to food/poison creation, what happens on death etc

--- SteeringBehaviorWithEvolution/sketch.js
+++ SteeringBehaviorWithEvolution/sketch.js
@@ -1,56 +1,90 @@
-let vehicles = [];
+let organisms = [];
 let food = [];
 let poison = [];
+let range;
 
 function setup() {
-  createCanvas(600, 600);
-  for (let i = 0; i < 10; i++) {
+  createCanvas(800, 400);
+  // create random organisms, food and poison
+  for (let i = 0; i < 50; i++) {
     let x = random(width);
     let y = random(height);
-    vehicles[i] = new Vehicle(x, y);
+    organisms[i] = new Organism(x, y);
   }
-
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 80; i++) {
     let x = random(width);
     let y = random(height);
     food.push(createVector(x, y));
   }
-
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 40; i++) {
     let x = random(width);
     let y = random(height);
     poison.push(createVector(x, y));
   }
+  createP("Organisms seek food (green) to sustain themselves, avoid poison (red) and have a chance to give birth to a child.")
+  range = createCheckbox("Senses of the organisms", false);
+  createP("Click on the canvas to create new random organism.");
+}
+
+// adding new random organism on click within the canvas
+function mouseClicked() {
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    organisms.push(new Organism(mouseX, mouseY));
+  }
 }
 
 function draw() {
-  background(51);
+  background(0);
 
-  if (random(1) < 0.1) {
+  // random chance to generate food
+  if (random(1) < 0.15) {
     let x = random(width);
     let y = random(height);
     food.push(createVector(x, y));
   }
 
+  // random chance to generate poison and remove the first one in array
+  if (random(1) < 0.05) {
+    let x = random(width);
+    let y = random(height);
+    poison.push(createVector(x, y));
+    if (random(1) < 0.7) {
+      poison.splice(0, 1);
+    }
+  }
+
+  // draw food and poison
   for (let i = 0; i < food.length; i++) {
-    fill(0, 255, 0);
+    fill(0, 255, 0, 150);
     noStroke();
-    ellipse(food[i].x, food[i].y, 8, 8);
+    ellipse(food[i].x, food[i].y, 4, 4);
   }
-
   for (let i = 0; i < poison.length; i++) {
-    fill(255, 0, 0);
+    fill(255, 0, 0, 150);
     noStroke();
-    ellipse(poison[i].x, poison[i].y, 8, 8);
+    ellipse(poison[i].x, poison[i].y, 4, 4);
   }
 
-  for (let i = vehicles.length - 1; i >= 0; i--) {
-    vehicles[i].behaviors(food, poison);
-    vehicles[i].update();
-    vehicles[i].display();
-
-    if (vehicles[i].dead()) {
-      vehicles.splice(i, 1);
+  // call organism functions
+  for (let i = organisms.length - 1; i >= 0; i--) {
+    organisms[i].boundaries();
+    organisms[i].behaviors(food, poison);
+    organisms[i].update();
+    organisms[i].display();
+    let child = organisms[i].mitosis();
+    if (child != null) {
+      organisms.push(child);
+    }
+    // create either food (70%) or poison (30%) on death
+    if (organisms[i].dead()) {
+      let x = organisms[i].position.x;
+      let y = organisms[i].position.y;
+      if (random(1) > 0.3) {
+        food.push(createVector(x, y));
+      } else {
+        poison.push(createVector(x, y));
+      }
+      organisms.splice(i, 1);
     }
   }
 }
