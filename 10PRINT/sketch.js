a226a35e08b3500de26b111a3a293c3ff1d1cc8d
Karol Fatyga <fatyga.karol@gmail.com>
2019-01-05 15:38:14 +0100

    back to black background (0)

--- 10PRINT/sketch.js
+++ 10PRINT/sketch.js
@@ -27,7 +27,7 @@ function resize() {
   resizeCanvas(img.width+offset, img.height+offset);
   userInput.position(0, height+5);
   p.position(0, height+15);
-  background(225);
+  background(0);
   slashes.x = 0;
   slashes.y = 0;
   active = true;
